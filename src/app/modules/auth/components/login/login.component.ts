f1352c3a04a102ed4ad8cf976262657af285e273
ko1ebayev <ko1ebayev.worx@gmail.com>
2023-06-21 00:30:44 +0300

    disable google

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -15,19 +15,19 @@ export class LoginComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    const handleResponse = (response: any) => {
-      console.log(response);
-    }
-    google.accounts.id.initialize({
-      client_id: "318603230447-6qjgeco2187epkkbtkid4tc5fv65efdg.apps.googleusercontent.com", // Replace with your Google Client ID
-      callback: handleResponse // We choose to handle the callback in client side, so we include a reference to a function that will handle the response
-    });
-    // You can skip the next instruction if you don't want to show the "Sign-in" button
-    google.accounts.id.renderButton(
-      document.getElementById("g_id_onload"), // Ensure the element exist and it is a div to display correcctly
-      { theme: "outline", size: "large" }  // Customization attributes
-    );
-    google.accounts.id.prompt();
+    // const handleResponse = (response: any) => {
+    //   console.log(response);
+    // }
+    // google.accounts.id.initialize({
+    //   client_id: "318603230447-6qjgeco2187epkkbtkid4tc5fv65efdg.apps.googleusercontent.com", // Replace with your Google Client ID
+    //   callback: handleResponse // We choose to handle the callback in client side, so we include a reference to a function that will handle the response
+    // });
+    // // You can skip the next instruction if you don't want to show the "Sign-in" button
+    // google.accounts.id.renderButton(
+    //   document.getElementById("g_id_onload"), // Ensure the element exist and it is a div to display correcctly
+    //   { theme: "outline", size: "large" }  // Customization attributes
+    // );
+    // google.accounts.id.prompt();
   }
 
   async login(provider: 'apple' | 'google'): Promise<void> {
@@ -36,7 +36,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
     }
 
     if (provider === 'google') {
-      google.accounts.id.prompt();
+      // google.accounts.id.prompt();
     }
   }
 }
