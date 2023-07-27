8bcac48cc4a51a9f30cefd80f820c8a5eb9514d0
ko1ebayev <ko1ebayev.worx@gmail.com>
2023-07-27 10:54:53 +0600

    add share logs button

--- src/app/modules/settings/components/settings-page/settings-page.component.ts
+++ src/app/modules/settings/components/settings-page/settings-page.component.ts
@@ -1,17 +1,34 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { AuthService } from 'src/app/services/auth.service';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { DarwinService } from "src/app/services/platform/darwin-platform.service";
 
 @Component({
-    selector: 'app-settings-page',
-    templateUrl: './settings-page.component.html',
-    styleUrls: ['./settings-page.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-settings-page",
+  templateUrl: "./settings-page.component.html",
+  styleUrls: ["./settings-page.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SettingsPageComponent {
-    public readonly authProvider = localStorage.getItem('auth_provider');
-    constructor(private readonly authService: AuthService) {}
+  public readonly authProvider = localStorage.getItem("auth_provider");
 
-    public signOut(): void {
-        this.authService.signOut();
-    }
+  constructor(
+    private readonly authService: AuthService,
+    private readonly darwinService: DarwinService
+  ) {}
+
+  public signOut(): void {
+    this.authService.signOut();
+  }
+
+  public shareLogs(): void {
+    const user = window.systemInfo().user.username;
+    this.darwinService
+      .executeCommand(
+        `mv /tmp/homevpn.log /Users/${user}/Desktop/ && install -m 777 /dev/null /tmp/homevpn.log`,
+        true
+      )
+      .then(() => {
+        alert("Done!");
+      });
+  }
 }
