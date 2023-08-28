767b58d60bbdb852fe5f7277bf0440330c0c6116
ko1ebayev <ko1ebayev.worx@gmail.com>
2023-08-28 15:47:22 +0600

    fix getDuration

--- src/app/modules/shop/shop-page/shop-page.component.ts
+++ src/app/modules/shop/shop-page/shop-page.component.ts
@@ -1,16 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { AppQuery } from 'src/app/state/app.query';
-import { AppService } from 'src/app/state/app.service';
-import { BehaviorSubject, switchMap } from 'rxjs';
-import {
-  Router,
-} from '@angular/router';
-import { CurrentPlan } from 'src/app/models/interfaces/current-plan.interface';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { AppQuery } from "src/app/state/app.query";
+import { AppService } from "src/app/state/app.service";
+import { BehaviorSubject, switchMap } from "rxjs";
+import { Router } from "@angular/router";
+import { CurrentPlan } from "src/app/models/interfaces/current-plan.interface";
 
 @Component({
-  selector: 'app-shop-page',
-  templateUrl: './shop-page.component.html',
-  styleUrls: ['./shop-page.component.scss'],
+  selector: "app-shop-page",
+  templateUrl: "./shop-page.component.html",
+  styleUrls: ["./shop-page.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ShopPageComponent {
@@ -18,72 +16,85 @@ export class ShopPageComponent {
 
   public readonly currentPlan$ = this.appQuery.currentPlan$;
 
-  public readonly planId$ = new BehaviorSubject<string>('');
+  public readonly planId$ = new BehaviorSubject<string>("");
 
   public readonly loading$ = new BehaviorSubject<boolean>(false);
 
   constructor(
     private readonly appQuery: AppQuery,
     private readonly appService: AppService,
-    private readonly router: Router,
+    private readonly router: Router
   ) {}
 
-  public getDuration(minutes: number): string {
-    const [hh, mm, ss] = new Date((minutes / 60) * 60 * 1000)
-      .toISOString()
-      .substr(11, 8)
-      .split(':');
-    let result = '';
+  public getDuration(seconds: number): string {
+    let totalMinutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+    let totalHours = Math.floor(totalMinutes / 60);
+    let remainingMinutes = totalMinutes % 60;
+    let days = Math.floor(totalHours / 24);
+    let hours = totalHours % 24;
 
-    if ((minutes / 60) >= 1440) {
-      result = `${minutes / 60 / 24} days`;
+    let result = "";
+
+    if (days) {
+      if (days === 1) {
+        result += `${days} day `;
+      } else {
+        result += `${days} days `;
+      }
     }
-    if (Number(ss)) {
-      result = `${Number(ss)} seconds`;
+    
+    if (hours) {
+      result += `${hours} hours `;
     }
-    if (Number(mm)) {
-      result = `${Number(mm)} minutes` + ' ' + result;
+    if (remainingMinutes) {
+      result += `${remainingMinutes} minutes `;
     }
-    if (Number(hh)) {
-      result = `${Number(hh)} hours` + ' ' + result;
+    if (remainingSeconds) {
+      result += `${remainingSeconds} seconds`;
     }
-    return result;
+
+    return result.trim();
   }
 
   public buy(): void {
     const planId = this.planId$.getValue();
-    const price = this.appQuery.plans.find(plan => plan.id === planId)?.price!;
+    const price = this.appQuery.plans.find((plan) => plan.id === planId)
+      ?.price!;
     const balance = this.appQuery.balance;
     const country = this.appQuery.country;
-    
+
     if (!country) {
       this.loading$.next(true);
       this.appService.purchasePlan(planId).subscribe(() => {
         this.loading$.next(false);
-        this.router.navigate(['/home']);
-      })
+        this.router.navigate(["/home"]);
+      });
       return;
     }
-    
+
     if (price > balance) {
-      alert('Insufficient balance!');
+      alert("Insufficient balance!");
       return;
     }
-    
+
     if (planId) {
       this.loading$.next(true);
-      this.appService.purchasePlan(planId).pipe(
-        switchMap(() => {
-          return this.appService.connectionInit();
-        }),
-        switchMap(() => {
-          return this.appService.wgUp();
-        })
-      ).subscribe(() => {
-        this.loading$.next(false);
-        this.appService.setConnection('on');
-        this.router.navigate(['/home']);
-      });
+      this.appService
+        .purchasePlan(planId)
+        .pipe(
+          switchMap(() => {
+            return this.appService.connectionInit();
+          }),
+          switchMap(() => {
+            return this.appService.wgUp();
+          })
+        )
+        .subscribe(() => {
+          this.loading$.next(false);
+          this.appService.setConnection("on");
+          this.router.navigate(["/home"]);
+        });
     }
   }
 
@@ -93,7 +104,7 @@ export class ShopPageComponent {
     }
 
     if (id === this.planId$.getValue()) {
-      this.planId$.next('');
+      this.planId$.next("");
       return;
     }
 
